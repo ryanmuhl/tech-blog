f1a6ba5bdbe754e123c9aa55d77a61d0ade18435
ryanmuhl <ryanmuhl@hotmail.com>
2022-06-06 16:40:09 -0400

    update post model

---
+++ models/Post.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/config');
+
+//Populate Item Model with Id, title, content, user_id
+class Post extends Model { }
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        content: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'User',
+                key: 'id',
+            }
+         
+        },
+
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post',
+    }
+    
+);
+
+module.exports = Item;
