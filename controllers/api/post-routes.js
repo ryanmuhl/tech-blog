bb1083b515ef92ec1d3aecd321c74d1f82b8d464
ryanmuhl <ryanmuhl@hotmail.com>
2022-06-06 17:16:05 -0400

    update post route get all posts

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,3 +1,14 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
+
+// / get all users http://localhost:3001/api/post/
+router.get('/', async (req, res) => {
+    Post.findAll({
+    })
+      .then(dbPostData => res.json(dbPostData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
