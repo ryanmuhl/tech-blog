1cf8503bf8586ec51711aab954576dd07ec04bd3
ryanmuhl <ryanmuhl@hotmail.com>
2022-06-11 09:39:57 -0400

    updte delete post route

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -73,4 +73,26 @@ router.post('/', withAuth, async (req, res) => {
     });
 });
 
+//Get post by id http://localhost:3001/api/post/id
+router.delete("/:id", withAuth, (req, res) => {
+  Post.destroy({
+          where: {
+              id: req.params.id,
+          },
+      })
+      .then((dbPostData) => {
+          if (!dbPostData) {
+              res.status(404).json({
+                  message: "No post found with this id"
+              });
+              return;
+          }
+          res.json(dbPostData);
+      })
+      .catch((err) => {
+          console.log(err);
+          res.status(500).json(err);
+      });
+});
+
 module.exports = router;
