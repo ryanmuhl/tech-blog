7b58e6d3bafe41efb0f914b4daed3e14db223acc
ryanmuhl <ryanmuhl@hotmail.com>
2022-06-11 10:31:25 -0400

    update navbar main.handlebars

---
+++ public/js/logout.js
@@ -0,0 +1,17 @@
+
+//Function to Fetch Logout Route.  If user is logged out
+//They will be re directed to the home page.
+const logout = async () => {
+    const response = await fetch('/api/user/logout', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+    });
+  
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert('Failed to log out.');
+    }
+  };
+  
+  document.querySelector('#logout').addEventListener('click', logout);
